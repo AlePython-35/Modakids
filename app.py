4dc52b5e6abd29383ee1dab3195dc8b6697cab0a
Racheldacosta <mariaalluizaa01@gmail.com>
2024-11-14 09:08:11 -0300

    Ajustes

--- app.py
+++ app.py
@@ -40,11 +40,20 @@ def start():
     cookie = request.cookies.get('usuario')
 
     if cookie:
-        # Se o cookie existe, Converte o valor dele de JSON para dicionário
+        # Se o cookie existe
+        # Converte o cookie JSON para um dicionário Python
         g.usuario = json.loads(cookie)
     else:
         # Se o cookie não existe, a variável do ususário está vazia
         g.usuario = ''
+
+    # # Cria um usuário "fake" para testes
+    # # No futuro, isso virá de um cookie
+    # g.usuario = {
+    #     'id': '1',
+    #     'nome': 'Joca da Silva',
+    #     'pnome': 'Joca',
+    # }
 
 
 @app.route("/")  # Rota raiz, equivalente a página inicial do site (index)
@@ -54,10 +63,9 @@ def index():  # Função executada ao acessar a rota raiz
     if g.usuario == '':
         return redirect(url_for('login'))
 
-    # Recebe valor da querystring, se existir → /?a=xxxxx
     acao = request.args.get('a')
 
-    # Obtém todos os 'trecos' do usuário conectado
+    # Um SQL de teste para exibir todos os 'trecos' do usuário conectado
     sql = '''
         SELECT t_id, t_foto, t_nome, t_descricao, t_localizacao
         FROM treco
@@ -67,17 +75,17 @@ def index():  # Função executada ao acessar a rota raiz
     '''
     cur = mysql.connection.cursor()
     cur.execute(sql, (g.usuario['id'],))
-    rows = cur.fetchall()
+    trecos = cur.fetchall()
     cur.close()
 
     # Teste de mesa para verificar o retorno dos dados do banco de dados
-    # print('\n\n\n DB:', rows, '\n\n\n')
+    # print('\n\n\n', trecos, '\n\n\n')
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
-        'titulo': 'CRUDTrecos',  # ← 'titulo' é obrigatório para todas as páginas / rotas
-        'usuario': g.usuario,  # ← 'usuario' é obrigatório para todas as páginas / rotas
-        'items': rows,
+        'titulo': 'CRUDTrecos',
+        'usuario': g.usuario,
+        'trecos': trecos,
         'acao': acao,
     }
 
@@ -104,15 +112,13 @@ def novo():  # Função executada para cadastrar novo treco
 
         # Teste de mesa (comente depois dos testes)
         # Verifica se os dados do formulário chegaram ao back-end
-        # print('\n\n\n FORM:', form, '\n\n\n')
+        # print('\n\n\n', form, '\n\n\n')
 
         # Grava os dados no banco de dados
         sql = '''
             INSERT INTO treco (
                 t_usuario, t_foto, t_nome, t_descricao, t_localizacao
-            ) VALUES (
-                %s, %s, %s, %s, %s
-            )
+            ) VALUES (%s, %s, %s, %s, %s)
         '''
         cur = mysql.connection.cursor()
         cur.execute(sql, (
@@ -170,7 +176,6 @@ def edita(id):
         mysql.connection.commit()
         cur.close()
 
-        # Após editar, retorna para a lista de itens
         return redirect(url_for('index', a='editado'))
 
     sql = '''
@@ -192,7 +197,7 @@ def edita(id):
     pagina = {
         'titulo': 'CRUDTrecos',
         'usuario': g.usuario,
-        'item': row,
+        'treco': row,
     }
 
     return render_template('edita.html', **pagina)
@@ -206,9 +211,8 @@ def apaga(id):
         return redirect(url_for('login'))
 
     # (des)comente o método para apagar conforme o seu caso
-    # Apaga completamente o treco (CUIDADO!)
-    # sql = 'DELETE FROM treco WHERE t_id = %s'
-    # Altera o status do treco para 'del' (Mais seguro)
+    # sql = 'DELETE FROM treco WHERE t_id = %s' # Apaga completamente o treco (CUIDADO!)
+    # Altera o status do treco para 'del'
     sql = "UPDATE treco SET t_status = 'del'  WHERE t_id = %s"
 
     # Executa o SQL
@@ -217,32 +221,35 @@ def apaga(id):
     mysql.connection.commit()
     cur.close()
 
-    # Retorna para a lista de items
+    # Retorna para a página anterior
     return redirect(url_for('index', a='apagado'))
 
 
 @app.route('/login', methods=['GET', 'POST'])  # Rota para login de usuário
 def login():
 
-    # Se o usuário está logado, redireciona para a página de perfil
+    # Verifica se o usuário está logado → Pelo cookie
     if g.usuario != '':
-        return redirect(url_for('perfil'))
+        # Se o usuário está logado
+        # Redireciona para a página inicial
+        return redirect(url_for('index'))
 
     erro = False
 
-    # Se o formulário foi enviado
     if request.method == 'POST':
+        # Se o formulário foi enviado
 
         # Pega os dados preenchidos no formulário
         form = dict(request.form)
 
         # Teste mesa
-        # print('\n\n\n FORM:', form, '\n\n\n')
+        # print('\n\n\nFORM:', form, '\n\n\n')
 
         # Pesquisa se os dados existem no banco de dados → usuario
+        # datetime.datetime(2024, 11, 8, 9, 23, 28)
         sql = '''
             SELECT *,
-                -- Gera uma versão das datas em pt-BR para salvar no cookie
+                -- Gera uma versão das datas em pt-BR
                 DATE_FORMAT(u_data, '%%d/%%m/%%Y às %%H:%%m') AS u_databr,
                 DATE_FORMAT(u_nascimento, '%%d/%%m/%%Y') AS u_nascimentobr
             FROM usuario
@@ -256,16 +263,17 @@ def login():
         cur.close()
 
         # Teste mesa
-        # print('\n\n\n DB:', usuario, '\n\n\n')
+        # print('\n\n\nDICT:', usuario, '\n\n\n')
 
         if usuario == None:
             # Se o usuário não foi encontrado
             erro = True
         else:
-            # Se achou o usuário, apaga a senha do usuário
+            # Se achou o usuário
+            # Apaga a senha do usuário para salvar no cookie
             del usuario['u_senha']
 
-            # Extrai o primeiro nome do usuário
+            # Primeiro nome do usuário
             usuario['u_pnome'] = usuario['u_nome'].split()[0]
 
             # Formata as datas para usar no JSON
@@ -274,17 +282,17 @@ def login():
             # Remove o prefixo das chaves do dicionário
             cookie_valor = remove_prefixo(usuario)
 
-            # Converte os dados em JSON (texto) para gravar no cookie,
-            # porque cookies só aceitam dados na forma texto
+            # Converte os dados em JSON (texto) para gravar no cookie
+            # Porque cookies só aceitam dados na forma de JSON
             cookie_json = json.dumps(cookie_valor)
 
             # Teste de mesa
-            # print('\n\n\n JSON:', cookie_json, '\n\n\n')
+            # print('\n\n\nCOOKIE:', cookie_json, '\n\n\n')
 
             # Prepara a página de destino → index
             resposta = make_response(redirect(url_for('index')))
 
-            # Cria o cookie
+            # Cria um cookie
             resposta.set_cookie(
                 key='usuario',  # Nome do cookie
                 value=cookie_json,  # Valor a ser gravado no cookie
@@ -310,10 +318,10 @@ def logout():
     if g.usuario == '':
         return redirect(url_for('login'))
 
-    # Página de destino após logout
+    # Página de destino de logout
     resposta = make_response(redirect(url_for('login')))
 
-    # Apaga o cookie do usuário
+    # apaga o cookie do usuário
     resposta.set_cookie(
         key='usuario',  # Nome do cookie
         value='',  # Apara o valor do cookie
@@ -327,54 +335,15 @@ def logout():
 @app.route('/cadastro', methods=['GET', 'POST'])  # Cadastro de usuário
 def cadastro():
 
-    jatem = ''
-    success = False
-
-    # Se o usuário está logado redireciona para a página de perfil
+    # Verifica se o usuário está logado → Pelo cookie
     if g.usuario != '':
-        return redirect(url_for('perfil'))
-
-    if request.method == 'POST':
-
-        form = dict(request.form)
-
-        # Verifica se usuário já está cadastrado, pelo e-mail
-        sql = "SELECT u_id, u_status FROM usuario WHERE u_email = %s AND u_status != 'del'"
-        cur = mysql.connection.cursor()
-        cur.execute(sql, (form['email'],))
-        rows = cur.fetchall()
-        cur.close()
-
-        # print('\n\n\n LEN:', len(rows), '\n\n\n')
-
-        if len(rows) > 0:
-            # Se já está cadastrado
-            if rows[0]['u_status'] == 'off':
-                jatem = 'Este e-mail já está cadastrado para um usuário inativo. Entre em contato para saber mais.'
-            else:
-                jatem = 'Este e-mail já está cadastrado. Tente fazer login ou solicitar uma nova senha.'
-        else:
-            # Se não está cadastrado, inclui os dados do form no banco de dados
-            sql = "INSERT INTO usuario (u_nome, u_nascimento, u_email, u_senha) VALUES (%s, %s, %s, SHA1(%s))"
-            cur = mysql.connection.cursor()
-            cur.execute(
-                sql, (
-                    form['nome'],
-                    form['nascimento'],
-                    form['email'],
-                    form['senha'],
-                )
-            )
-            mysql.connection.commit()
-            cur.close()
-
-            success = True
+        # Se o usuário está logado
+        # Redireciona para a página inicial
+        return redirect(url_for('index'))
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
         'titulo': 'CRUDTrecos - Cadastre-se',
-        'jatem': jatem,
-        'success': success,
     }
 
     return render_template('cadastro.html', **pagina)
@@ -386,9 +355,11 @@ def novasenha():
     novasenha = ''
     erro = False
 
-    # Se o usuário está logado, redireciona para a página de perfil
+    # Verifica se o usuário está logado → Pelo cookie
     if g.usuario != '':
-        return redirect(url_for('perfil'))
+        # Se o usuário está logado
+        # Redireciona para a página inicial
+        return redirect(url_for('index'))
 
     # Se o formulário foi enviado
     if request.method == 'POST':
@@ -397,7 +368,7 @@ def novasenha():
         form = dict(request.form)
 
         # Teste de mesa
-        # print('\n\n\n FORM:', form, '\n\n\n')
+        # print('\n\n\nFORM:', form, '\n\n\n')
 
         # Pesquisa pelo email e nascimento informados, no banco de dados
         sql = '''
@@ -413,18 +384,23 @@ def novasenha():
         cur.close()
 
         # Teste de mesa
-        # print('\n\n\n DB:', row, '\n\n\n')
+        # print('\n\n\nDB:', row, '\n\n\n')
 
-        # Se o usuário não existe
+        # Se o usuário existe
         if row == None:
-            # Exibe mensagem no frontend
+
             erro = True
+
         else:
             # Gera uma nova senha
             novasenha = gerar_senha()
 
             # Salva a nova senha no banco de dados
-            sql = "UPDATE usuario SET u_senha = SHA1(%s) WHERE u_id = %s"
+            sql = '''
+                UPDATE usuario
+                SET u_senha = SHA1(%s)
+                WHERE u_id = %s
+            '''
             cur = mysql.connection.cursor()
             cur.execute(sql, (novasenha, row['u_id'],))
             mysql.connection.commit()
@@ -450,23 +426,27 @@ def perfil():
     # Calcula idade do usuário
     g.usuario['idade'] = calcular_idade(g.usuario['nascimento'])
 
-    # Obtém a quantidade de trecos ativos do usuário
-    sql = "SELECT count(t_id) AS total FROM treco WHERE t_usuario = %s AND t_status = 'on'"
+    # Obtém a quantidade de trecos ativos os usuário
+    sql = '''
+        SELECT count(t_id) AS total
+        FROM treco
+        WHERE t_usuario = %s
+            AND t_status = 'on'
+    '''
     cur = mysql.connection.cursor()
     cur.execute(sql, (g.usuario['id'],))
     row = cur.fetchone()
     cur.close()
 
     # Teste de mesa
-    # print('\n\n\n DB', row, '\n\n\n')
+    # print('\n\n\n', row, '\n\n\n')
 
-    # Adiciona a quantidade ao perfil
     g.usuario['total'] = row['total']
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
         'titulo': 'CRUDTrecos - Novo Treco',
-        'usuario': g.usuario,
+        'usuario': g.usuario,  # Dados do cookie do usuário
     }
 
     # Renderiza o template HTML, passaod valores para ele
@@ -489,7 +469,7 @@ def apagausuario():
     mysql.connection.commit()
     cur.close()
 
-    # Configura o status dos itens do usuário para 'del' no banco de dados
+    # Configura o status dos trecos do usuário para 'del' no banco de dados
     sql = "UPDATE treco SET t_status = 'del' WHERE t_usuario = %s"
     cur = mysql.connection.cursor()
     cur.execute(sql, (g.usuario['id'],))
